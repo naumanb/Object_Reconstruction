65d060104c1c595dea105099ad7f9bd4869b7699
Nauman Baig <naumanmbaig@gmail.com>
2025-03-17 01:30:04 -0400

    fix: updated eval.py to run for Windows fix: updated shell scipts EOF syntax for linux

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 cd BundleSDF
-export LD_LIBRARY_PATH=/opt/conda/envs/py38/lib/
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:/opt/conda/envs/py38/lib/
 python run_custom.py
